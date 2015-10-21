602e6acba467c2aee88736c44d5725441df40040
Rico Sennrich <rico.sennrich@gmx.ch>
2015-10-21 12:19:38 +0100

    also convert state when not using rolling_dict (for noise and possibly more)

--- experiments/nmt/utils/convert_state_local.py
+++ experiments/nmt/utils/convert_state_local.py
@@ -8,11 +8,13 @@ args = parser.parse_args()
 
 with open(args.state,'rb') as f:
     d = cPickle.load(f)
-d['n_sym_source'] = d['large_vocab_source']
-d['n_sym_target'] = d['large_vocab_target']
-del d['large_vocab_source']
-del d['large_vocab_target']
-del d['rolling_vocab']
+
+if 'rolling_vocab' in d and d['rolling_vocab']
+    d['n_sym_source'] = d['large_vocab_source']
+    d['n_sym_target'] = d['large_vocab_target']
+    del d['large_vocab_source']
+    del d['large_vocab_target']
+    del d['rolling_vocab']
 del d['save_algo']
 del d['save_gs']
 d['weight_noise'] = False
